bdf46a762a36ed47f7c4b325095bd22317df8f98
rcerf <rickcerf@gmail.com>
2013-10-14 15:23:21 -0700

    modularized out request handler from basic-server

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,6 +4,38 @@
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
 
-var handleRequest = function(request, response) {
+exports.handleRequest = function(request, response) {
+  /* This is the callback function that will be called each time a
+ * client (i.e.. a web browser) makes a request to our server. */
 
+  /* Request is an http.ServerRequest object containing various data
+   * about the client request - such as what URL the browser is
+   * requesting. */
+  console.log("Serving request type " + request.method + " for url " + request.url);
+
+  /* "Status code" and "headers" are HTTP concepts that you can
+   * research on the web as and when it becomes necessary. */
+  var statusCode = 200;
+
+  var server = require('./basic-server.js');
+
+  /* Without this line, this server wouldn't work.  See the note
+   * below about CORS. */
+  var headers = server.defaultCorsHeaders;
+
+  headers['Content-Type'] = "text/plain";
+
+  /* Response is an http.ServerRespone object containing methods for
+   * writing our response to the client. Documentation for both request
+   * and response can be found at
+   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html*/
+  response.writeHead(statusCode, headers);
+  /* .writeHead() tells our server what HTTP status code to send back
+   * to the client, and what headers to include on the response. */
+
+  /* Make sure to always call response.end() - Node will not send
+   * anything back to the client until you do. The string you pass to
+   * response.end() will be the body of the response - i.e. what shows
+   * up in the browser.*/
+  response.end("Hello, World!!");
 };
