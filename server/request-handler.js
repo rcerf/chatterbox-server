bc8fede1b39722e5a1c167c6bc73533ecf83570c
rcerf <rickcerf@gmail.com>
2013-10-14 15:48:26 -0700

    recognizes URL "1/classes/chatterbox"

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,30 +12,33 @@ exports.handleRequest = function(request, response) {
    * about the client request - such as what URL the browser is
    * requesting. */
   console.log("Serving request type " + request.method + " for url " + request.url);
+  if(request.url === "/1/classes/chatterbox"){
+    /* "Status code" and "headers" are HTTP concepts that you can
+     * research on the web as and when it becomes necessary. */
+    var statusCode = 200;
 
-  /* "Status code" and "headers" are HTTP concepts that you can
-   * research on the web as and when it becomes necessary. */
-  var statusCode = 200;
+    var server = require('./basic-server.js');
 
-  var server = require('./basic-server.js');
+    /* Without this line, this server wouldn't work.  See the note
+     * below about CORS. */
+    var headers = server.defaultCorsHeaders;
 
-  /* Without this line, this server wouldn't work.  See the note
-   * below about CORS. */
-  var headers = server.defaultCorsHeaders;
+    headers['Content-Type'] = "text/plain";
 
-  headers['Content-Type'] = "text/plain";
+    /* Response is an http.ServerRespone object containing methods for
+     * writing our response to the client. Documentation for both request
+     * and response can be found at
+     * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html*/
+    response.writeHead(statusCode, headers);
+    /* .writeHead() tells our server what HTTP status code to send back
+     * to the client, and what headers to include on the response. */
 
-  /* Response is an http.ServerRespone object containing methods for
-   * writing our response to the client. Documentation for both request
-   * and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html*/
-  response.writeHead(statusCode, headers);
-  /* .writeHead() tells our server what HTTP status code to send back
-   * to the client, and what headers to include on the response. */
-
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.end("Hello, World!!");
+    /* Make sure to always call response.end() - Node will not send
+     * anything back to the client until you do. The string you pass to
+     * response.end() will be the body of the response - i.e. what shows
+     * up in the browser.*/
+    response.end("Hello, World!! " + request.url);
+  }else{
+    response.end("URL not recognized.");
+  }
 };
