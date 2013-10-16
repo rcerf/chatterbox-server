646c519c7bc7b2e3c2fa5da30f93f14fde69b7bc
rcerf <rickcerf@gmail.com>
2013-10-15 17:33:46 -0700

    modularized code on working node server. GETs POSTs OPTIONs.

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,9 +1,37 @@
-/* You should implement your request handler function in this file.
- * But you need to pass the function to http.createServer() in
- * basic-server.js.  So you must figure out how to export the function
- * from this file and include it in basic-server.js. Check out the
- * node module documentation at http://nodejs.org/api/modules.html. */
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  'Content-Type': "application/json"
+};
+var idCounter = 1;
+var messages = [];
 
-var handleRequest = function(request, response) {
+var sendResponse = function(response, data, status){
+  status = status || 200;
+  response.writeHead(status, headers);
+  response.end(JSON.stringify(data));
+};
 
+var collectData = function(response){
+  sendResponse(response, messages, 200);
+};
+
+var storeData = function(request, response){
+  request.on('data', function(data){
+    messages.push(JSON.parse(data));
+  });
+  console.log(request.method);
+  sendResponse(response, null, 201);
+};
+
+module.exports = handleRequest = function(request, response) {
+  var httpVerb = {
+    'GET': function() { collectData(response); },
+    'POST': function() { storeData(request, response); },
+    'OPTIONS': function() { sendResponse(response, null, 200); }
+  };
+
+  httpVerb[request.method]();
 };
