7b1ca226abe297e3fdd29b6ca5be84a6e43200b2
rcerf <rickcerf@gmail.com>
2013-10-15 20:53:00 -0700

    loads on localhost and saves messages in a text file

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -5,10 +7,35 @@ var headers = {
   "access-control-max-age": 10, // Seconds.
   'Content-Type': "application/json"
 };
-var idCounter = 1;
+
 var messages = [];
 
-var sendResponse = function(response, data, status){
+var extend = function(obj, source) {
+  for (var prop in source) {
+    obj[prop] = source[prop];
+  }
+  return obj;
+};
+
+module.exports.loadMessages = loadMessasges = function(){
+    messages = fs.readFileSync('./messages.txt', 'utf8');
+    messages = messages || '[]';
+    messages = JSON.parse(messages);
+};
+
+module.exports.loadFile = loadFile = function(response, fileName, contentType){
+  fs.readFile(fileName, function (err, data) {
+    if (err) throw err;
+    var newHeaders = {};
+    extend(newHeaders, headers);
+    newHeaders['Content-Type'] = contentType;
+    response.writeHeader(200, newHeaders);
+    response.write(data);
+    response.end();
+  });
+};
+
+module.exports.sendResponse = sendResponse = function(response, data, status){
   status = status || 200;
   response.writeHead(status, headers);
   response.end(JSON.stringify(data));
@@ -19,14 +46,21 @@ var collectData = function(response){
 };
 
 var storeData = function(request, response){
-  request.on('data', function(data){
-    messages.push(JSON.parse(data));
+  var data = "";
+
+  request.on('data', function(chunk){
+    data += chunk;
   });
-  console.log(request.method);
+
+  request.on('end', function(){
+    messages.push(JSON.parse(data));
+    fs.writeFileSync('./messages.txt', JSON.stringify(messages));
+  });
+
   sendResponse(response, null, 201);
 };
 
-module.exports = handleRequest = function(request, response) {
+module.exports.handleRequest = handleRequest = function(request, response) {
   var httpVerb = {
     'GET': function() { collectData(response); },
     'POST': function() { storeData(request, response); },
